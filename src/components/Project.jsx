c3adc1218df1b73f7d5aca5034755f873aba9429
warriorBunny013 <mona23sonai@gmail.com>
2023-05-25 23:59:35 +0530

    ui imp

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,4 +1,4 @@
-
+import Tilt from 'react-parallax-tilt'
 import { ComputersCanvas } from "./canvas";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
@@ -9,7 +9,7 @@ const Project = () => {
         <div className="md:pl-20 md:pr-20 pt-20 sm:pl-10 sm:pr-10 min-h-screen ">   
            <div className="flex justify-center flex-wrap gap-20">
            {/* <span className="font-bold tracking-widest">FEATURED PROJECT</span> */}
-       
+         <Tilt>
           <div data-aos="fade-up-right" className="max-w-sm border font-mono overflow-hidden">
   <img  className="w-full" src="https://user-images.githubusercontent.com/97738453/237886571-d9d6c585-e4c9-47ee-8c92-a2087a5c5c9b.png" alt="Sunset in the mountains"/>
   <div className="px-6 py-4">
@@ -27,6 +27,8 @@ const Project = () => {
         
            </div>
              </div>
+             </Tilt>
+             <Tilt>
           <div data-aos="fade-up-left" className="max-w-sm border font-mono overflow-hidden">
   <img className="w-full" src="https://user-images.githubusercontent.com/97738453/237698574-3982043c-595e-47e0-b398-78bab9b9612d.png" alt="Sunset in the mountains"/>
   <div className="px-6 py-4">
@@ -44,6 +46,7 @@ const Project = () => {
         
            </div>
              </div>
+             </Tilt>
           </div>
         
        </div>   
@@ -52,7 +55,7 @@ const Project = () => {
         <div className="md:pl-20 md:pr-20 pt-20 sm:pl-10 sm:pr-10 min-h-screen  ">   
            <div className="flex justify-center flex-wrap gap-20">
            {/* <span className="font-bold tracking-widest">FEATURED PROJECT</span> */}
-       
+         <Tilt>
           <div data-aos="fade-up-right" className="max-w-sm border font-mono overflow-hidden">
   <img className="w-full" src="chatify.png" alt="Sunset in the mountains"/>
   <div className="px-6 py-4">
@@ -70,23 +73,71 @@ const Project = () => {
         
            </div>
              </div>
+             </Tilt>
+             <Tilt>
           <div data-aos="fade-up-left" className="max-w-sm border font-mono overflow-hidden">
-  <img className="w-full" src="orchid.png" alt="Sunset in the mountains"/>
+  <img className="w-full" src="student-db.png" alt="Sunset in the mountains"/>
   <div className="px-6 py-4">
-  <div className="text-xl z-10 font-mono font-bold">01</div>
-    <div className="font-bold text-3xl mb-2">ORCHID</div>
+  <div className="text-xl z-10 font-mono font-bold">05</div>
+    <div className="font-bold text-3xl mb-2">STORE DB</div>
   
    
-    <p className="z-10 font-mono opacity-75">Youtube streaming application with personalised chatbot and create secret rooms to watch videos with your friends</p>
+    <p className="z-10 font-mono opacity-75">A web application to authenticate admin users, store and export student database in form of .csv</p>
            
   </div>
   <div className="m-2 flex gap-2 w-1/2 items-center">
-           <button className="cursor-pointer mt-5 px-3 py-1 hover:bg-transparent hover:animate-pulse border-2 w-full bg-white text-black font-bold hover:text-white">Live</button>
+           <a href="https://drive.google.com/file/d/10DY6If143hWy5W5pTN6-Q-dTm4D7cZGl/view?usp=sharing"><button className="cursor-pointer mt-5 px-3 py-1 hover:bg-transparent hover:animate-pulse border-2 w-full bg-white text-black font-bold hover:text-white">Live</button></a>
         
-           <button className="cursor-pointer mt-5 px-3 py-1 hover:bg-cyan-400 hover:animate-pulse border-2 w-full">Github</button>
+           <a href="http://github.com/warriorBunny013/StudentDB-DentalKart"><button className="cursor-pointer mt-5 px-3 py-1 hover:bg-cyan-400 hover:animate-pulse border-2 w-full">Github</button></a>
         
            </div>
              </div>
+             </Tilt>
+          </div>
+        
+       </div>      
+        
+        <div className="md:pl-20 md:pr-20 pt-20 sm:pl-10 sm:pr-10 min-h-screen  ">   
+           <div className="flex justify-center flex-wrap gap-20">
+           {/* <span className="font-bold tracking-widest">FEATURED PROJECT</span> */}
+          <Tilt>
+          <div data-aos="fade-up-right" className="max-w-sm border font-mono overflow-hidden">
+  <img className="w-full" src="sportozo.png" alt="Sunset in the mountains"/>
+  <div className="px-6 py-4">
+  <div className="text-xl z-10 font-mono font-bold">06</div>
+    <div className="font-bold text-3xl mb-2">SPORTOZO</div>
+  
+   
+    <p className="z-10 font-mono opacity-75">Group chating application that allows the user's socialize without the need to revealing name</p>
+           
+  </div>
+  <div className="m-2 flex gap-2 w-1/2 items-center">
+           <a href="https://sportozo-uditi.netlify.app/"><button className="cursor-pointer mt-5 px-3 py-1 hover:bg-transparent hover:animate-pulse border-2 w-full bg-white text-black font-bold hover:text-white">Live</button></a>
+        
+           <a href="https://github.com/warriorBunny013/PLAYERS-DBMS-PROJECT-1"><button className="cursor-pointer mt-5 px-3 py-1 hover:bg-cyan-400 hover:animate-pulse border-2 w-full">Github</button></a>
+        
+           </div>
+             </div>
+             </Tilt>
+<Tilt>
+<div data-aos="fade-up-left" className="max-w-sm border font-mono overflow-hidden">
+  <img className="w-full" src="lookup.png" alt="Sunset in the mountains"/>
+  <div className="px-6 py-4">
+  <div className="text-xl z-10 font-mono font-bold">07</div>
+    <div className="font-bold text-3xl mb-2">LOOKUP</div>
+  
+   
+    <p className="z-10 font-mono opacity-75">A web application to store a reading wishlist in local with automatic image generation.</p>
+           
+  </div>
+  <div className="m-2 flex gap-2 w-1/2 items-center">
+           <a href="https://github.com/warriorBunny013/lookup"><button className="cursor-pointer mt-5 px-3 py-1 hover:bg-transparent hover:animate-pulse border-2 w-full bg-white text-black font-bold hover:text-white">Live</button></a>
+        
+           <a href="https://github.com/warriorBunny013/lookup"><button className="cursor-pointer mt-5 px-3 py-1 hover:bg-cyan-400 hover:animate-pulse border-2 w-full">Github</button></a>
+        
+           </div>
+             </div>
+             </Tilt>
           </div>
         
        </div>      
